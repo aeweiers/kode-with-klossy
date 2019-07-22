6988f0fb8a53f44b03799701f3b24a150a7bb0e6
Abbey Weiers <aeweiers@gmai.com>
2019-07-22 13:56:40 -0400

    my first playground

---
+++ Day1KWK.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+str
+
+var name = "Abbey"
+name
+name = "Abigail"
+name
+
+print("Hello World!")
+
+var age = "21"
+print(age)
+print("I am \(age)")
+//interpolate a variable by using \()
