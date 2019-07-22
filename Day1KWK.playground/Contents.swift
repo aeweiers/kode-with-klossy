af62daadc3b3d7e0fc76705ce25890edff7cc6b0
Abbey Weiers <aeweiers@gmai.com>
2019-07-22 14:25:17 -0400

    added a conditional

--- Day1KWK.playground/Contents.swift
+++ Day1KWK.playground/Contents.swift
@@ -14,3 +14,14 @@ var age = "21"
 print(age)
 print("I am \(age)")
 //interpolate a variable by using \()
+
+
+var gitHub = "Rocks"
+
+if gitHub == "Rocks" {
+    print("you did it!")
+}
+
+else {
+    print("try again")
+}
